71bb036a18fd9f538fba0105b5a719d5ad2d7383
Basem Gamal <bassemgamalosman12@gmail.com>
2024-01-20 22:23:16 -0800

    Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def best_score(a_dictionary):
+
+    key_list = list(a_dictionary.keys())
+    key = ""
+    pointer = 0
+    for i in key_list:
+        if a_dictionary[i] > pointer
+        pointer = a_dictionary[i]
+        key = i
+    return key
