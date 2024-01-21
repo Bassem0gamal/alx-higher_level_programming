b850de43775429adc232c778b0329a9d7d4ce426
Basem Gamal <bassemgamalosman12@gmail.com>
2024-01-20 22:25:20 -0800

    Test

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,12 @@
 
 def best_score(a_dictionary):
 
-    key_list = list(a_dictionary.keys())
-    key = ""
-    pointer = 0
-    for i in key_list:
-        if a_dictionary[i] > pointer
-        pointer = a_dictionary[i]
-        key = i
-    return key
+    if a_dictionary:
+         key_list = list(a_dictionary.keys())
+         key = ""
+         pointer = 0
+         for i in key_list:
+             if a_dictionary[i] > pointer
+             pointer = a_dictionary[i]
+             key = i
+         return key
