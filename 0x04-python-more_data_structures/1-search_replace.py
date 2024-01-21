67d938f67fac168e37ca0119cc8cc6f008df17c4
Basem Gamal <bassemgamalosman12@gmail.com>
2024-01-20 21:09:32 -0800

    Test

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@ def search_replace(my_list, search, replace):
 
     new_list = []
     for num in my_list:
-        if num == replace:
+        if num == search:
             new_list.append(replace)
         else:
             new_list.append(num)
