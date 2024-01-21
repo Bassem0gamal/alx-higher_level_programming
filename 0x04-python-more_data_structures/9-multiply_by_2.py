bdefcf39db55860e9531b66e800366d15f775f16
Basem Gamal <bassemgamalosman12@gmail.com>
2024-01-20 22:11:23 -0800

    Tist

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,6 @@
 def multiply_by_2(a_dictionary):
 
     new_dict = []
-    for key, value in a_dictionary.items():
-        new_dict.update({key: (value * 2)})
+    for key in a_dictionary:
+        new_dict[i] = a_dictionary[i] * 2
     return new_dict
