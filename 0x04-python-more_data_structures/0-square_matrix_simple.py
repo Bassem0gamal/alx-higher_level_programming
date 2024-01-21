a8ffaf3413f04b3f666296301322581447b0513e
Basem Gamal <bassemgamalosman12@gmail.com>
2024-01-20 20:52:50 -0800

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    #a function that computes the square value of all integers of a matrix
-
+    """
+    a function that computes the square value of all integers of a matrix
+    """
+    
     new_matrix = []
     for i in matrix:
         result = list(map(lambada x: x**2, i))
