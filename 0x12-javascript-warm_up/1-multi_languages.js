b224c518940988fca52a606d12a668ea30f0f634
Basem Gamal <bassemgamalosman12@gmail.com>
2024-03-12 06:30:14 -0700

    Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const firstLine = 'C is fun';
+const secLine = 'Python is cool';
+const therdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secLine);
+console.log(therdLine);
