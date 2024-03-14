1f5ae430082883ee7f984046d49c19624e8d6b19
Basem Gamal <bassemgamalosman12@gmail.com>
2024-03-13 19:27:10 -0700

    Test

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+const list = require('./100-data.js');
 console.log(list);
 const i = list.map((item, index) => item * index);
 console.log(i);
