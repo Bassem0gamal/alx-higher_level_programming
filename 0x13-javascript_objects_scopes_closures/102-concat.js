9eed0969cfe7193c4dc0be16c09f65be7f9dbaea
Basem Gamal <bassemgamalosman12@gmail.com>
2024-03-13 19:41:20 -0700

    Test

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const file = require('file');
+const fs = require('fs');
 
 const firstF = file.readFileSync(process.argv[2]).toString();
 const secondF = file.readFileSync(process.argv[3]).toString();
