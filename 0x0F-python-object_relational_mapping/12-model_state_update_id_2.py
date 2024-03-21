98b5061fe66b21dcae4feed2ecbb8c5b0ab10a26
Basem Gamal <bassemgamalosman12@gmail.com>
2024-03-21 08:49:02 -0700

    Task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script changes the name of a State object
+from the database
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Updates a State object on the database
+    """
+ 
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_ins = session.query(State).filter(State.id == '2').first()
+    new_ins.name = 'New Mexico'
+    session.commit()
+    session.close()
