cabab374fcbdeee2f613007be130accb960e09b5
Basem Gamal <bassemgamalosman12@gmail.com>
2024-03-31 16:53:08 -0700

    T

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,11 +3,18 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
- 
+
+    n = len(list_of_integers)
     if not list_of_integers:
         return None
+    if (n == 1):
+        return list_of_integers[0]
+    if (list_of_integers[0] >= list_of_integers[1]):
+        return list_of_integers[0]
+    if (list_of_integers[n - 1] >= list_of_integers[n - 2]):
+        return list_of_integers[n - 1]
 
     for i in range(1, len(list_of_integers) -1):
-        if list_of_integers[i] > list_of_integers[i - 1] and list_of_integers[i] > list_of_integers[i + 1]:
+        if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]:
             return list_of_integers[i]
         return None
