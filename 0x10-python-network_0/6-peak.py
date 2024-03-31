c8c43269ecb6cf156a4cb9108e5218a76e9be913
Basem Gamal <bassemgamalosman12@gmail.com>
2024-03-31 15:45:26 -0700

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers """
+
+def find_peak(list_of_integers):
+
+    if not list_of_integers:
+        return None
+
+    for i in range(1, len(list_of_integers) -1):
+        if list_of_integers[i] > list_of_integers[i - 1] and list_of_integers[i] > list_of_integers[i + 1]:
+            return list_of_integers[i]
+        return list_of_integers[0]
