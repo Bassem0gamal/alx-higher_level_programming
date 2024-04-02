7e30946773dd7e8f9924da63c0b7aefd11e2cfd8
Basem Gamal <bassemgamalosman12@gmail.com>
2024-04-01 17:16:32 -0700

    Task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,7 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    request = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(request) as res:
-        print(dict(res.headers).get("X-Request-Id"))
+    email = {"email": sys.argv[2]}
+    request = requests.post(sys.argv[1], data = email)
+
+    print(request.txet)
