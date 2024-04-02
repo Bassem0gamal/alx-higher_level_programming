c5e4bfd62a7abe31f68465e2c606ef2f65db72e5
Basem Gamal <bassemgamalosman12@gmail.com>
2024-04-01 17:07:13 -0700

    Task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 """
+Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    request = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(request) as res:
+        print(dict(res.headers).get("X-Request-Id"))
