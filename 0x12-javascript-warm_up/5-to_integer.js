b3afa3de34fe4673f69228b1b6b9ee73c4e5d4b5
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-04-09 12:41:04 -0700

    Old Task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,6 @@
 const converted = Number(process.argv[2]);
 if (isNaN(converted)) {
   console.log('Not a number');
-} else { console.log(`My number: ${converted}`);
-       }
+} else {
+  console.log(`My number: ${converted}`);
+}
