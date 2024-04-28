98480702d4079324edd9e76e9d67b8b256e6af8e
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-04-28 08:24:18 -0700

    Task 8

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+const API_URL = 'https://swapi-api.alx-tools.com/api/films/:id';
 
 request(API_URL + process.argv[2], function (err, response, body) {
   if (err) {
